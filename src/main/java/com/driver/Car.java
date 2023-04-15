b0ec94213e4c675dbe1106eaa624dc65c5295da5
Sindhura55 <117762932+Sindhura55@users.noreply.github.com>
2023-04-15 08:23:45 +0000

    task 1-Updated driver code of each class

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -11,14 +11,24 @@ public class Car extends Vehicle {
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+    this.name=name;
+    this.wheels=wheels;
+    this.doors=doors;
+    this.gears=gears;
+    this.isManual=isManual;
+    this.currentGear=1;
+    this.type=type;
+    this.seats=seats;
     }
 
     public void changeGear(int newGear){
+        currentGear=newGear;
 
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
+        
 
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,13 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        this.name=name;
+        this.isManual=isManual;
+
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = currentSpeed+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,7 +22,10 @@ public class F1 extends Car {
          */
 
         if(newSpeed == 0) {
+            stop();
+            changeGear(1);
             //Stop the car, set gear as 1
+
         }
         //for all other cases, change the gear accordingly
 
