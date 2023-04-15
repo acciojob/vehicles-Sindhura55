234dca07dbdb24005ec23ebac379e647f26c6b16
Sindhura55 <117762932+Sindhura55@users.noreply.github.com>
2023-04-15 10:50:15 +0000

    updated the method body of interface methods

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,3 +4,20 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+    public Boat(String name,int capacity)
+    {
+        this.name=name;
+        this.capacity=capacity;
+    }
+    public String getVehicleName()
+    {
+        return this.name;
+    }
+    public int getVehicleCapacity()
+    {
+        return this.capacity;
+    }
+}
