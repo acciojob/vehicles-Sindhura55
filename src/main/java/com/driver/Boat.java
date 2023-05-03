9279a9295a668ab42f794579a6275ffedf91439e
sindhura <sindhura343434@gmail.com>
2023-05-04 01:22:16 +0530

    new changes

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,29 @@
+package com.driver;
+
+public abstract class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+    public Boat(String name,int capacity){
+        this.name=name;
+        this.capacity=capacity;
+
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setCapacity(int capacity){
+        this.capacity=capacity;
+    }
+
+
+
+    public String getVehicleName(){
+        return this.name;
+    }
+
+    public int getCapacity()
+        {
+        return this.capacity ;
+    }
+
+}
