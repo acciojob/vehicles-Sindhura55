28c7791c1c27ec6bedbf1ab68e5e63cc2aa5a3b3
sindhura <sindhura343434@gmail.com>
2023-05-03 23:30:05 +0530

    Revert "updated the method body of interface methods"
    This reverts commit 234dca07dbdb24005ec23ebac379e647f26c6b16.

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,20 +4,3 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
-class Boat implements WaterVehicle{
-    String name;
-    int capacity;
-    public Boat(String name,int capacity)
-    {
-        this.name=name;
-        this.capacity=capacity;
-    }
-    public String getVehicleName()
-    {
-        return this.name;
-    }
-    public int getVehicleCapacity()
-    {
-        return this.capacity;
-    }
-}
